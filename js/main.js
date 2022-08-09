4da5788e529f8058f3028b5a26f9a71d14e99578
Timóteo Freires de Souza Bentes <bentestimoteo@gmail.com>
2022-08-09 17:34:16 -0300

    vai

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+//     $("html").niceScroll();
+// });
+
+$('.nav a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+
+    $('html, body').animate({
+        scrollTop: targetOffset - 100
+    }, 500);
+});
